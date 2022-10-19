0f6de9f0e95346305c1e089d12a59e1fee7f79de
10playgrounds <10playgrounds@gmail.com>
2022-10-19 18:18:34 +0900

    함수 호출방식, 인수, 반환값, 내장함수, 모듈, 성적처리프로그램 v5

--- 23func.py
+++ 23func.py
@@ -142,5 +142,24 @@ checkCredit(code)
 
 
 # ex) 60갑자를 출력해주는 프로그램을 함수로 작성
-# 함수명 : checkChinaZodiac(년도)
+# 함수명 : checkChinaZodiac(년도) # 4-123p
+
+def checkChinaZodiac(year):
+    baseYear = 1444
+    ten = ('갑','을','병','정','무',
+           '기','경','신','임','계')
+    twelve = ('자','축','인','묘','진','사',
+              '오','미','신','유','술','해')
+    animal = ('쥐','소','호랑이','토끼','용','뱀',
+              '말','양','원숭이','닭','개','돼지')
+
+    t10idx = (year - baseYear) % 10
+    t12idx = (year - baseYear) % 12
+
+    print(f'{year}년은 {ten[t10idx]}{twelve[t12idx]}년' 
+          f'({animal[t12idx]}의 해)입니다')
+
+
+year = int(input('년도는?'))
+checkChinaZodiac(year)
 
---
+++ 27sungjukv4c.py
@@ -0,0 +1,119 @@
+# 함수를 이용한 성적처리 프로그램
+
+# 함수 정의부
+def displayMenu():
+    main_menu = f'''
+    성적 처리프로그램 v4
+    -------------------
+    1. 성적 데이터 추가
+    2. 성적 데이터 조회
+    3. 성적 데이터 상세조회
+    4. 성적 데이터 수정
+    5. 성적 데이터 삭제
+    0. 프로그램 종료
+    -------------------'''
+    print(main_menu)
+
+
+def addSungJuk():
+    name = input('이름은?')
+    kor = int(input('국어는?'))
+    eng = int(input('영어는?'))
+    mat = int(input('수학은?'))
+
+    # 입력받은 성적데이터 초기화
+    sj = {'name': name, 'kor': kor, 'eng': eng, 'mat': mat}
+    computeSungJuk(sj)
+
+    sjs.append(sj)
+
+
+def readSungJuk():
+    hdr = '이름 국어 영어 수학\n'
+    hdr += '-----------------'
+    print(hdr)
+
+    for sj in sjs:
+        print(f'{sj["name"]} {sj["kor"]} {sj["eng"]} {sj["mat"]}')
+
+
+def readOneSungJuk():
+    name = input('조회할 학생의 이름은?')
+
+    sj = None
+    for item in sjs:  # 입력한 이름과 일치하는 항목을 sjs에서 찾음
+        if item['name'] == name: sj = item
+
+    hdr = '이름 국어 영어 수학 총점 평균 학점\n'
+    hdr += '-------------------------------'
+    print(hdr)
+    for k in sj.keys():
+        print(sj.get(k), end=' ')
+
+
+def modifySungJuk():
+    name = input('수정할 학생 이름은?')
+
+    idx = None
+    for i in range(len(sjs)):  # 입력한 이름과 일치하는 항목을 sjs에서 찾음
+        if sjs[i]['name'] == name:
+            idx = i
+            break
+
+    # 새로운(기존) 값을 입력받음
+    kor = int(input(f'새로운 국어는? ({sjs[idx]["kor"]})'))
+    eng = int(input(f'새로운 영어는? ({sjs[idx]["eng"]})'))
+    mat = int(input(f'새로운 수학은? ({sjs[idx]["mat"]})'))
+
+    # 다시 성적 처리
+    sj = {'name': name, 'kor': kor, 'eng': eng, 'mat': mat}
+    computeSungJuk(sj)
+
+    # 기존 위치에 다시 저장
+    sjs[idx] = sj
+
+
+def removeSungJuk():
+    name = input('삭제할 데이터의 학생 이름은?')
+
+    idx = None
+    for i in range(len(sjs)):  # 삭제할 데이터의 인덱스 조사
+        item = sjs[i]
+        if item['name'] == name: idx = i
+
+    sjs.pop(idx)
+
+
+def computeSungJuk(sj):
+    tot = sj['kor'] + sj['eng'] + sj['mat']
+    avg = tot / 3
+    grd = '가'
+    if avg >= 90: grd = '수'
+    elif avg >= 80: grd = '우'
+    elif avg >= 70: grd = '미'
+    elif avg >= 60: grd = '양'
+
+    sj['tot'] = tot
+    sj['avg'] = avg
+    sj['grd'] = grd
+
+
+# 성적 데이터 저장할 변수 선언
+sjs = []
+
+# 프로그램 주 실행부
+while True:
+    # 메뉴 표시 및 값 입력
+    displayMenu()
+    menu = input('=> 메뉴를 선택하세요 : ')
+
+    if menu == '1': addSungJuk()
+    elif menu == '2': readSungJuk()
+    elif menu == '3': readOneSungJuk()
+    elif menu == '4': modifySungJuk()
+    elif menu == '5': removeSungJuk()
+    elif menu == '0': break
+    else: print('잘못된 번호를 입력했습니다!')
+
+
+
